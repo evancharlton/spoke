62a8bfb495cb30247664a01eb81559275b2f0549
Evan Charlton <evancharlton@gmail.com>
2024-12-08 20:16:59 +0100

    feat: Add additional (pruned) word lists
    Add some `-1` variants to `nb` and `nn` which are pruned dictionaries.
    
    These are not exposed in the UI.

--- build-words.sh
+++ build-words.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 
-mkdir -p public/nb/
-mkdir -p public/nn/
+mkdir -p public/nb{,-1}/
+mkdir -p public/nn{,-1}/
 
-node wordlist/spoke \
+jq -c < wordlist/spoke/nb-1/trie.json > public/nb-1/trie.json \
+  || exit $?
+jq -c < wordlist/spoke/nn-1/trie.json > public/nn-1/trie.json \
   || exit $?
 jq -c < wordlist/spoke/nb/trie.json > public/nb/trie.json \
   || exit $?
