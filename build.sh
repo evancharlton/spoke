03e790e714bd89989f0d28ee6f1ba3444f86c519
Evan Charlton <evancharlton@gmail.com>
2025-01-08 20:57:20 +0100

    fix: Corrupted build.sh file

--- build.sh
+++ build.sh
@@ -6,10 +6,3 @@ npm run build \
   || exit $?
 
 echo "xn--spke-hra.no" >> dist/CNAME
- wordlists ..."
-find wordlist/stavehumle \
-  -mindepth 1 \
-  -maxdepth 1 \
-  -type d \
-  -exec cp -Rv {} public/ \; \
-|| exit $?
